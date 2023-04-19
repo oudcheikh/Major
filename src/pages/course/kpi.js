c09a3f0872a0b69fdb7d25d2ad7bd1c3a713c84e
Mahmoud dev <appmajor.cours@gmail.com>
2023-04-19 18:03:19 +0200

    improve

--- src/pages/course/kpi.js
+++ src/pages/course/kpi.js
@@ -1,61 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { LineChart, Line } from "recharts";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { query, getDocs, where, getDoc, doc,collectionGroup ,onSnapshot,collection, orderBy, Timestamp} from "firebase/firestore";
-import { auth, logInWithEmailAndPassword, signInWithGoogle, logout, db } from "../../firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
 import Grid from '@mui/material/Grid';
+import { collection, onSnapshot, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Bar, BarChart, CartesianGrid, ComposedChart, Legend, Line, Tooltip, XAxis, YAxis } from "recharts";
+import { auth, db } from "../../firebase";
 
-const data = [
-  {
-    "coursAnnuleClient": 0,
-    "coursNoteParent": 0,
-    "coursNoteProf": 0,
-    "coursAnnuleAdmin": 0,
-    "coursNoteEleve": 0,
-    "eleveInscrit": 0,
-    "parentInscrit": 0,
-    "ProfActif": 166,
-    "date": {
-        "seconds": 1677110400,
-        "nanoseconds": 0
-    },
-    "coursTermine": 0,
-    "coursAssign": 0,
-    "coursProfAbsent": 0,
-    "coursNonTermine": 0,
-    "coursAnnuleProfCritique": 0,
-    "coursSansProf": 0,
-    "ProfInscrit": 2,
-    "coursPris": 0,
-    "coursAnnuleProfTotal": 0,
-    "uid": "2023_02_23"
-  },
-  {
-    "coursSansProf": 0,
-    "coursNoteProf": 0,
-    "coursTermine": 0,
-    "ProfActif": 166,
-    "coursAnnuleProfCritique": 0,
-    "coursNoteParent": 0,
-    "coursAnnuleProfTotal": 0,
-    "ProfInscrit": 1,
-    "coursAnnuleClient": 0,
-    "coursNoteEleve": 0,
-    "coursPris": 0,
-    "coursAnnuleAdmin": 0,
-    "coursAssign": 0,
-    "coursProfAbsent": 0,
-    "parentInscrit": 0,
-    "eleveInscrit": 0,
-    "date": {
-        "seconds": 1677196800,
-        "nanoseconds": 0
-    },
-    "coursNonTermine": 0,
-    "uid": "2023_02_24"
-  }
-];
 
 const Chart = () => {
 
@@ -66,10 +15,6 @@ const Chart = () => {
   const [allclientssecond, setAlltClientsecond] = useState([])
   let [allprofs, setAllprofs] = useState([])
  
-
-  
-
-
 
   const fetchAllClient = async () => {
 
@@ -114,30 +59,94 @@ const Chart = () => {
     // </LineChart>
 
     <div>
-    <Grid sx={12}> 
-        <BarChart
+    <Grid sx={6}> 
+        <ComposedChart
           width={1500}
-          height={500}
+          height={600}
           data={allclientssecond}
           margin={{
             top: 20,
-            right: 30,
+            right: 20,
+            bottom: 20,
             left: 20,
-            bottom: 5,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="uid" />
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="uid"  />
           <YAxis />
           <Tooltip />
           <Legend />
+          
           <Bar dataKey="ProfInscrit" fill="#8884d8" />
           <Bar dataKey="parentInscrit" fill="#82ca9d" />
           <Bar dataKey="eleveInscrit" fill="#82ca9e" />
-        </BarChart>
+          <Line type="monotone" dataKey="eleveInscrit" stroke="#ff7300" />
+          <Line type="monotone" dataKey="ProfInscrit" stroke="#8884d8" />
+        </ComposedChart>
         </Grid>
 
-<Grid sx={12}> 
+
+<Grid sx={6}> 
+
+<ComposedChart
+          width={1500}
+          height={600}
+          data={allclientssecond}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="uid"  />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          
+          <Bar dataKey="coursProfAbsent" fill="#82ca6c" />
+  <Bar dataKey="coursNonTermine" fill="#82ca9g" />
+  <Bar dataKey="coursAnnuleProfCritique" fill="#32ca5d" />
+  <Bar dataKey="coursSansProf" fill="#85ca9d" />
+  <Bar dataKey="coursAnnuleClient" fill="#384627" />
+  <Bar dataKey="coursAnnuleAdmin" fill="#764614" />
+  <Bar dataKey="coursAnnuleProfTotal" fill="#773614" />
+          <Line type="monotone" dataKey="coursAnnuleClient" stroke="#ff7300" />
+        </ComposedChart>
+
+
+
+
+
+</Grid>
+
+<Grid sx={6}>
+<ComposedChart
+          width={1500}
+          height={600}
+          data={allclientssecond}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="uid"  />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          
+          <Bar dataKey="coursAssign" barSize={20} fill="#82ca9d" />
+          <Bar dataKey="coursPris" barSize={20} fill="#783627" />
+          <Bar dataKey="coursTermine" barSize={20} fill="#8884d8" />
+          <Line type="monotone" dataKey="coursPris" stroke="#ff7300" />
+          <Line type="monotone" dataKey="coursTermine" stroke="#8884d8" />
+        </ComposedChart>
+        </Grid>
+        <Grid sx={6}> 
 <BarChart
   width={1500}
   height={500}
@@ -154,52 +163,11 @@ const Chart = () => {
   <YAxis />
   <Tooltip />
   <Legend />
-  <Bar dataKey="coursTermine" fill="#8884d8" />
   <Bar dataKey="coursNoteProf" fill="#8562d8" />
   <Bar dataKey="coursNoteEleve" fill="#8511d1" />
   <Bar dataKey="coursNoteParent" fill="#8252d3" />
-  <Bar dataKey="coursAssign" fill="#82ca9d" />
-  <Bar dataKey="coursProfAbsent" fill="#82ca6c" />
-  <Bar dataKey="coursNonTermine" fill="#82ca9g" />
-  <Bar dataKey="coursAnnuleProfCritique" fill="#32ca5d" />
-  <Bar dataKey="coursSansProf" fill="#85ca9d" />
-  <Bar dataKey="coursPris" fill="#783627" />
-  <Bar dataKey="coursAnnuleClient" fill="#384627" />
-  <Bar dataKey="coursAnnuleAdmin" fill="#764614" />
-  <Bar dataKey="coursAnnuleProfTotal" fill="#773614" />
-  <Bar dataKey="coursAssign" fill="#785731" />
-
-
-  "coursTermine": 0,
-    "coursAssign": 0,
-    "coursProfAbsent": 0,
-    "coursNonTermine": 0,
-    "coursAnnuleProfCritique": 0,
-    "coursSansProf": 0,
-    "ProfInscrit": 2,
-    "coursPris": 0,
-
-    "coursSansProf": 0,
-    "coursNoteProf": 0,
-    "coursTermine": 0,
-    "ProfActif": 166,
-    "coursAnnuleProfCritique": 0,
-    "coursNoteParent": 0,
-    "coursAnnuleProfTotal": 0,
-    "ProfInscrit": 1,
-    "coursAnnuleClient": 0,
-    "coursNoteEleve": 0,
-    "coursPris": 0,
-    "coursAnnuleAdmin": 0,
-    "coursAssign": 0,
-    "coursProfAbsent": 0,
-    "parentInscrit": 0,
-    "eleveInscrit": 0,
-    
-    "coursNonTermine": 0,
-
-
 </BarChart>
+
 </Grid>
 </div>
       
